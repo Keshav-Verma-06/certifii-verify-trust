342b5364dbe3b0f7cd29d9f179ecb82e376eba86
Keshav Verma <keshav.verma@vit.edu.in>
2025-09-08 18:39:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: ":4523c66e-ef94-47f3-ab7d-90e3fb5f75a8-00-2kodia72pua09.worf.replit.dev:",
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
